224b145be846fe4f0805f35ac21b6c5fa54330b0
komma <sweethome026@yahoo.co.jp>
2020-04-14 15:10:04 +0900

    サイドバーのカテゴリ一覧実装、テンプレートタグによりHTMLが出力されるので scss書き直し

--- sidebar.php
+++ sidebar.php
@@ -1,108 +1,21 @@
 <aside class="blog-sidebar">
 	<section class="widget category">
 		<h4 class="widget__ttl category__ttl">カテゴリー</h4>
+
+
 		<ul class="category__lists">
-			<li class="category__lists__item">
-				<a class="category__name">Illustrator<span class="entry-count">
-				#
-				</a>
-				<ul class="category__contents">
-					<li class="category__contents__item">
-						<a href="#">Illustrator<span class="entry-count">
-						<?php
-							$chosen_id = 11; // カテゴリID
-							$thisCat = get_category($chosen_id);
-							echo $thisCat->count;
-						?></span>
-					</a>
-					</li>
-					<li class="category__contents__item">
-						<a href="#">超初心者のためのイラストレーター講座<span class="entry-count">
-							<?php
-								$chosen_id = 12; // カテゴリID
-								$thisCat = get_category($chosen_id);
-								echo $thisCat->count;
-							?>
-						</span>
-						</a>
-					</li>
-				</ul>
-			</li>
-			<li class="category__lists__item">
-				<a class="category__name">WordPress<span class="entry-count">
-				<?php
-					$chosen_id = 5; // カテゴリID
-					$thisCat = get_category($chosen_id);
-					echo $thisCat->count;
-				?>
-				</span>
-				</a>
-			</li>
-			<li class="category__lists__item">
-				<a class="category__name">サイト構築<span class="entry-count">
-				<?php
-					$chosen_id = 7; // カテゴリID
-					$thisCat = get_category($chosen_id);
-					echo $thisCat->count;
-					?>
-					</span>
-				<ul class="category__contents">
-					<li class="category__contents__item">
-						<a href="#">HTML<span class="entry-count">
-						<?php
-							$chosen_id = 13; // カテゴリID
-							$thisCat = get_category($chosen_id);
-							echo $thisCat->count;
-						?>
-						</span>
-					</li>
-					<li class="category__contents__item">
-						<a href="#">CSS<span class="entry-count">
-						<?php
-							$chosen_id = 14; // カテゴリID
-							$thisCat = get_category($chosen_id);
-							echo $thisCat->count;
-						?>
-						</span>
-					</li>
-					<li class="category__contents__item">
-						<a href="#">JavaScript<span class="entry-count">
-						<?php
-							$chosen_id = 15; // カテゴリID
-							$thisCat = get_category($chosen_id);
-							echo $thisCat->count;
-						?>
-						</span>
-					</li>
-				</ul>
-			</li>
-			<li class="category__lists__item">
-				<a class="category__name">Others<span class="entry-count">
-				<?php
-					$chosen_id = 8; // カテゴリID
-					$thisCat = get_category($chosen_id);
-					echo $thisCat->count;
-				?>
-				</span>
-			</li>
-			<li class="category__lists__item">
-				<a class="category__name">フリーランス<span class="entry-count">
-				<?php
-					$chosen_id = 9; // カテゴリID
-					$thisCat = get_category($chosen_id);
-					echo $thisCat->count;
-				?>
-				</span>
-			</li>
-			<li class="category__lists__item">
-				<a class="category__name">Days<span class="entry-count">
-				<?php
-					$chosen_id = 10; // カテゴリID
-					$thisCat = get_category($chosen_id);
-					echo $thisCat->count;
-				?>
-					</span>
-			</li>
+
+			
+		<?php // タームの一覧を表示
+			$catlist = wp_list_categories(array(
+				'title_li' => '', // リストの外側に表示されるタイトルを非表示
+				'show_count' => 1, // カテゴリの投稿数を表示
+				'echo' => 0 // 設定した値を返す
+			));
+			$catlist = preg_replace('/<\/a> (\([0-9]*\))/', ' <span class="entry-count">$1</span></a>', $catlist); // 投稿数をタグで囲う
+			$catlist = str_replace(array('(',')'), '', $catlist); // 投稿数を囲う（）を削除
+			echo $catlist; // タームの一覧を表示
+		?>
 		</ul><!-- //category__lists -->
 	</section><!-- //widget category -->
 
